2bfcc065a21ebd6d47670b62471823c2734e5a28
Tay Weida <weida.tay99@gmail.com>
2021-08-14 02:51:01 +0800

    update css styles to bem modifiers for confirmation

--- src/vbs/pages/ConfirmationPage/ConfirmationPage.js
+++ src/vbs/pages/ConfirmationPage/ConfirmationPage.js
@@ -5,21 +5,21 @@ import StatusBar from "../../shared/StatusBar";
 
 const ConfirmationPage = () => {
   return (
-    <div className="confirmationContainer">
+    <div className="confirmation__container">
       <StatusBar stage={4}/>
-      <div className="submitContainer">
-        <div className="submitContainer-text">
-          <h2 className="submitContainer-title">Submitted!</h2>
+      <div className="submit__container">
+        <div className="submit__container__textbox">
+          <h2 className="submit__container__title">Submitted!</h2>
           <p>A receipt of your venue booking will be sent to the email provided.</p>
           <p>The status of your booking will be updated in 3-5 working days through the same email address.</p>
           <p>Thank you for your patience!</p>
         </div>
       </div>
-      <div className="bottomNavigation">
-        <Link className="confirmationHomeButton confirmationHomeButtonMobile" to="/">
+      <div className="navigation__bottom">
+        <Link className="confirmation__button__home confirmation__button__home--mobile" to="/">
           HOME
         </Link>
-        <Link className="confirmationNewButton confirmationNewButtonMobile" to="/vbs">
+        <Link className="confirmation__button__new confirmation__button__new--mobile" to="/vbs">
           NEW BOOKING
         </Link>
       </div>
